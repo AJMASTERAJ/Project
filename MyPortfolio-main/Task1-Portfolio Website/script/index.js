efc2eb184b6ba5425da3aeea7426d14136f6c829
Abhishek Dandegavkar <117561711+AJMASTERAJ@users.noreply.github.com>
2023-05-16 16:16:03 +0530

    Add files via upload

---
+++ MyPortfolio-main/Task1-Portfolio Website/script/index.js
@@ -0,0 +1,10 @@
+let typed = new Typed(".typing", {
+    strings: [ "",
+      " Abhishek Jain",
+      " Web Devloper",
+      
+    ],
+    typeSpeed: 150,
+    backSpeed: 80,
+    loop: true,
+  });
